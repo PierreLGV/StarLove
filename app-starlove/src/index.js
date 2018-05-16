1b5a1d54a5efc0917a6e26c2abb8b1498e0d9a3e
Capucine Leclerc <capucine_49@hotmail.com>
2018-05-16 19:56:08 +0200

    create folder

--- app-starlove/src/index.js
+++ app-starlove/src/index.js
@@ -5,4 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Intro />, document.getElementById('intro'));
+
 registerServiceWorker();
