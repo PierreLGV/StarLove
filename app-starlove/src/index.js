26202496a25be11843658770e0df2c07cde4be80
Capucine Leclerc <capucine_49@hotmail.com>
2018-05-16 20:46:52 +0200

    created component intro

--- app-starlove/src/index.js
+++ app-starlove/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Intro from './components/intro.js';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Intro />, document.getElementById('intro'));
 
-registerServiceWorker();
+//registerServiceWorker();
