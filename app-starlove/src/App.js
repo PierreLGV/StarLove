a3fc0f44c5069c54cb4dc7f364ebd0fcd8ed00c1
Pierre Langevin <pierrelangev1@gmail.com>
2018-05-16 19:43:18 +0200

    intro starwars scroll

--- app-starlove/src/App.js
+++ app-starlove/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './intro.css';
+
 
 class App extends Component {
   render() {
@@ -13,8 +15,26 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      
+      <div className="fade"></div>
+
+      <div className="star-wars">
+        <div className="crawl">
+          <div className="title">
+            <p>Episode IV</p>
+            <h1>A New Hope</h1>
+          </div>
+          
+          <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
+            
+            <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
+      
+          <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy….</p>
+        </div>
       </div>
-    );
+      </div>
+      
+    )
   }
 }
 
