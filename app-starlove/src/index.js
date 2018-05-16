46fa1ecccc25b4c884251a32b16406f74d0f36e3
Pierre Langevin <pierrelangev1@gmail.com>
2018-05-16 22:02:27 +0200

    page intro ok

--- app-starlove/src/index.js
+++ app-starlove/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Intro from './components/intro.js';
 //import registerServiceWorker from './registerServiceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Intro />, document.getElementById('intro'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 //registerServiceWorker();
