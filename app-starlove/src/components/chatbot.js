c4b5fe2e993b5267109a2661a37e9ba855d2ce5f
Christian LIM <christian.lim@outlook.fr>
2018-05-18 00:00:53 +0200

    Screen + chat

--- app-starlove/src/components/chatbot.js
+++ app-starlove/src/components/chatbot.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Chat from '../chatWindow.png'
+import './windowChat.css'
 
 const HandleChat = ({ handleClientLine, ifChoiceA, ifChoiceB, ifChoiceC, choiceA, choiceB, choiceC, text }) =>
     <div>
+      <img src={Chat} className="window" alt="chatWindow"></img>
       <p>{text}</p>
       <button onClick={() => handleClientLine(ifChoiceA)}>{choiceA}</button>
       <button onClick={() => handleClientLine(ifChoiceB)}>{choiceB}</button>
