19bf3a5575b50b24ceccad6c70a5df4c3def2625
Louise Foussat <louisefoussat@gmail.com>
2018-05-18 13:45:46 +0200

    Best counter EVER

--- app-starlove/src/App.js
+++ app-starlove/src/App.js
@@ -60,6 +60,7 @@ class App extends Component {
       image: 'https://image.noelshack.com/fichiers/2018/20/5/1526608297-akabab.jpg'
       },
     credit: 0,
+    callStart: 0,
     chatLines: [
       {
         text: 'Tu viens d\'où?',
@@ -148,8 +149,8 @@ class App extends Component {
   }
 
   hangUpCall = () => {
-    clearInterval(this.counter)
     this.setState({ status: '', credit: 0, currentStep: 0, currentList: this.state.characters })
+    console.log(this.state.credit)
   }
 
   handleAnswer = filter => {
@@ -161,7 +162,11 @@ class App extends Component {
 
   chooseBitch = bitch => {
     console.log('got it');
-    this.setState({ status: 'chatting', currentList: bitch })
+    this.setState({
+      status: 'chatting',
+      currentList: bitch,
+      callStart: Date.now()
+    })
   }
 
   handleClientLine = () => {
@@ -169,9 +174,6 @@ class App extends Component {
       currentLine: this.state.currentLine + 1,
     })
   }
-
-  addCredit = () => this.setState({credit: this.state.credit += 1})
-  counter = () => setInterval(this.addCredit, 1000)
 
   constructor() {
     super()
@@ -212,16 +214,16 @@ class App extends Component {
 
   chatting() {
     if (this.state.currentLine < this.state.chatLines.length) {
-        this.counter()
-        return <div>
-          <MoneyMoneyMoney credit={this.state.credit} />
+      return (
+        <div>
+          <MoneyMoneyMoney callStart={this.state.callStart} />
           <ShowBitch character={this.state.currentList} />
           <HandleChat handleClientLine={this.handleClientLine}
-          {...this.state.chatLines[this.state.currentLine]} />
+            {...this.state.chatLines[this.state.currentLine]} />
         </div>
+      )
       }
-      clearInterval(this.counter)
-      this.setState({ status: '', credit: 0 })
+      this.setState({ status: '' })
   }
 
   render() {
@@ -239,9 +241,7 @@ class App extends Component {
     return (
       <div>
         <Cockpit handleCall={this.handleCall} hangUpCall={this.hangUpCall}/>
-        {typeof this[this.state.status] === 'function'
-          ? this[this.state.status]()
-          : undefined}
+        {this[this.state.status]()}
       </div>
     )
   }
--- app-starlove/src/components/moneyMoneyMoney.js
+++ app-starlove/src/components/moneyMoneyMoney.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './count.css'
 
-const MoneyMoneyMoney = ({ credit }) =>
-    <div className="count">
-      <p><span>Sold :</span> {credit}<span> Ȼ$</span></p>
-    </div>
+
+class MoneyMoneyMoney extends Component {
+  loop = () => {
+    this.forceUpdate()
+    this.requestId = requestAnimationFrame(this.loop)
+  }
+
+  getCredit = () => ((Date.now() - this.props.callStart) / 1000).toFixed(2)
+
+  componentDidMount() {
+    this.loop()
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.requestId)
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className="count">
+        <p><span>Sold :</span> {this.getCredit()}<span> Ȼ$</span></p>
+      </div>
+    )
+  }
+}
 
 export default MoneyMoneyMoney
