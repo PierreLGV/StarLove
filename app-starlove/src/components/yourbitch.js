7f2121af9e39d7da3f94fb541961e38231d2d118
Christian LIM <christian.lim@outlook.fr>
2018-05-18 08:13:44 +0200

    Working progress 2

--- app-starlove/src/components/yourbitch.js
+++ app-starlove/src/components/yourbitch.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './yourbitch.css'
 
 const ShowBitch = ({ character }) =>
-    <div>
+    <div className="bitch">
       <img src={character.image} alt={character.name} />
-      <p>{character.name}</p>
-      <p>{character.gender}</p>
-      <p>{character.height}</p>
-      <p>{character.mass}</p>
+      <p><span>name: </span>{character.name}</p>
+      <p><span>gender: </span>{character.gender}</p>
+      <p><span>height: </span>{character.height}</p>
+      <p><span>mass: </span>{character.mass}</p>
     </div>
 
 export default ShowBitch
