579cf4e62aa1e0915248e4a64266c6317438ea44
Christian LIM <christian.lim@outlook.fr>
2018-05-18 03:28:59 +0200

    bug ecran résolu

--- app-starlove/src/components/questions.js
+++ app-starlove/src/components/questions.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Chat from '../chatWindow.png'
+import './windowChat.css'
 
 const HandleQuestion = ({ handleAnswer, ifyes, ifno, yes, no, text }) =>
     <div>
+      <img src={Chat} className="window" alt="chatWindow"></img>
       <p>{text}</p>
       <button onClick={() => handleAnswer(ifyes)}>{yes}</button>
       <button onClick={() => handleAnswer(ifno)}>{no}</button>
